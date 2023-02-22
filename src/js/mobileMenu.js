e9927da28238d0a0f5bfa55a3f1b135175067566
YuraFLex <shvalov.yurec@gmail.com>
2023-02-22 14:32:08 +0200

    рефактор мобілного меню js

--- src/js/mobileMenu.js
+++ src/js/mobileMenu.js
@@ -1,16 +1,15 @@
 // Открытие/заыкртиые мобильного меню
 
-(() => {
-    const refs = {
-      openMobileBtn: document.querySelector('[data-menu-open]'),
-      closeMobileBtn: document.querySelector('[data-menu-close]'),
-      mobile: document.querySelector('[data-menu]'),
-    };
-  
-    refs.openMobileBtn.addEventListener('click', toggleMobile);
-    refs.closeMobileBtn.addEventListener('click', toggleMobile);
-  
-    function toggleMobile() {
-      refs.mobile.classList.toggle('is-open');
-    }
-  })();
+const refs = {
+  openMobileBtn: document.querySelector('[data-menu-open]'),
+  closeMobileBtn: document.querySelector('[data-menu-close]'),
+  mobile: document.querySelector('[data-menu]'),
+};
+
+refs.openMobileBtn.addEventListener('click', toggleMobile);
+refs.closeMobileBtn.addEventListener('click', toggleMobile);
+
+function toggleMobile(event) {
+  event.preventDefault();
+  refs.mobile.classList.toggle('is-open');
+}
